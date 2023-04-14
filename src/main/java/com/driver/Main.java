44a0f7bcf3d6cd5f37a43e60524ab60b78f0ebc9
USER <9493saikrishna@gmail.com>
2023-04-14 23:27:49 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly rwOnly = new RWOnly();
+
+      rwOnly.setName("Saikrsna");
+      String value = rwOnly.getName();
+
+      System.out.println(value);
+  }
 }
